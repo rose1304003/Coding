b2e42c3be506311e720696e82c74945eeb15059c
Rosie <axmedovarobiya16@gmail.com>
2026-01-14 23:55:21 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -173,7 +173,7 @@ def main():
     application.add_handler(CommandHandler("notify_hackathon", notify_hackathon_command))
     application.add_handler(CommandHandler("download", download_submission_command))
     application.add_handler(CommandHandler("submissions", list_submissions_command))
-    app.add_handler(CommandHandler("export_files", export_all_files))
+   
 
     
     # Callback handler
